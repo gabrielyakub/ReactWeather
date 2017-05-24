a8b7860a7403a7c1176e6dc4874fd76eef1f13be
Gabriel Yakub <gabrielyakub@gmail.com>
2017-05-24 19:23:10 +0700

    Prep repo for heroku

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ var express = require('express');
 // Create app
 var app = express();
 
+// port dari env var
+const PORT = process.env.PORT || 3000;
+
+// openweather cuma kerja di HTTP gk di HTTPS
+//express middleware
+app.use(function(req, res, next){
+  //request, response, next(buat redirect kalo middlewarenya selesai)
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://'+ req.hostname + req.url);
+  }
+})
+
 // app.use() add functionality to express application
 // express.static -> specify folder name to be exposed to the web server
 app.use(express.static('public'));
 
 // start the server, app.listen(port, function(){});
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function(){
+  console.log('Express server is up on port ' + PORT);
 });
